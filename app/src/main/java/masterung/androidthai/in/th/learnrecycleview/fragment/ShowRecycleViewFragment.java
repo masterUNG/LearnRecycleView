56422ae0eac9cab0507032bf606f1162656198a4
MasterUNG <phrombutr@gmail.com>
2018-02-12 21:42:30 +0700

    ToolBar

--- app/src/main/java/masterung/androidthai/in/th/learnrecycleview/fragment/ShowRecycleViewFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/learnrecycleview/fragment/ShowRecycleViewFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.learnrecycleview.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.learnrecycleview.MainActivity;
 import masterung.androidthai.in.th.learnrecycleview.R;
 
 /**
@@ -14,6 +16,28 @@ import masterung.androidthai.in.th.learnrecycleview.R;
  */
 
 public class ShowRecycleViewFragment extends Fragment {
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarShowRecycleView);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle("Show RecycleView");
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Create by RecycleView");
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+    }   // Main Method
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
